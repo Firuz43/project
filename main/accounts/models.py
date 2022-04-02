1cc6f05c4b5c39e49146b9b662e4b12a68cf683a
Firuz43 <firuz1199@mail.ru>
2022-04-02 12:42:25 +0400

    changing Tag

--- main/accounts/models.py
+++ main/accounts/models.py
@@ -14,6 +14,14 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+
+    name = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -24,14 +32,7 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
-
-
-class Tag(models.Model):
-
-    name = models.CharField(max_length=100, null=True)
-
-    def __str__(self):
-        return self.name
+    tags = models.ManyToManyField(Tag)
 
 
 class Order(models.Model):
@@ -45,4 +46,3 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    tags = models.ManyToManyField(Tag)
