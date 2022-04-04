a951f1c12e381a38931ce782160a43dc7ce5adee
Firuz43 <firuz1199@mail.ru>
2022-04-04 15:41:47 +0400

    customer changes

--- main/accounts/urls.py
+++ main/accounts/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('customer', views.customer, name="customer"),
-    path('products', views.products, name="products"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk>/', views.customer, name="customer"),
+
 ]
