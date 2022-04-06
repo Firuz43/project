356940c0c6d1570387cb37e1304e8b77385cac6c
Firuz43 <firuz1199@mail.ru>
2022-04-06 22:53:36 +0400

    url changes

--- main/accounts/urls.py
+++ main/accounts/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
     path('customer/<str:pk>/', views.customer, name="customer"),
-
 ]
