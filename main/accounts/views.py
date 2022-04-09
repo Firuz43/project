8a76aa3ac2466841ba2bcdadc1226bf76ef3238b
Firuz43 <firuz1199@mail.ru>
2022-04-10 00:06:31 +0400

    User login and register page

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from .forms import OrderForm
 # Create your views here.
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 
 def home(request):
@@ -37,8 +49,8 @@ def products(request):
     return render(request, 'accounts/products.html', {'products': products})
 
 
-def createOrder(request):
-
+def createOrder(request, pk):
+    customer = Customer.objects.get(id=pk)
     form = OrderForm()
     if request.method == 'POST':
         form = OrderForm(request.POST)
